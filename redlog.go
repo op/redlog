5007affc556b10aa56a719016ede74d87b0c3db5
Örjan Fors <o@42mm.org>
2024-05-11 22:10:29 +0200

    chore: document public variables

--- redlog.go
+++ redlog.go
@@ -9,9 +9,20 @@ import (
 // Catppuccin consists of three adaptive variants mixed from Catppuccin's
 // beautiful pastel palettes.
 var Catppuccin = struct {
-	Frappe    *log.Styles
+	// Frappe is an less vibrant alternative using subdued colors for a muted
+	// aesthetic. It is an adaptive variant using Latte in light mode and Frappe
+	// elsewhere.
+	Frappe *log.Styles
+
+	// Macchiato has medium contrast with gentle colors creating a soothing
+	// atmosphere. It is an adaptive variant using Latte in light mode
+	// and Macchiato elsewhere.
 	Macchiato *log.Styles
-	Mocha     *log.Styles
+
+	// Mocha is the original and the darkest variant, offering a cozy feeling
+	// with color-rich accents. It is an adaptive variant using Latte in light
+	// mode and Mocha elsewhere.
+	Mocha *log.Styles
 }{
 	Frappe:    newCatppuccin(catppuccin.Latte, catppuccin.Frappe),
 	Macchiato: newCatppuccin(catppuccin.Latte, catppuccin.Macchiato),
